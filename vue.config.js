9f86f57c5789704c5b92f161c3f42f9ce6fe3caa
Sandro Weber <webers@in.tum.de>
2021-05-30 23:26:16 +0200

    msg-formats 0.15.12

--- vue.config.js
+++ vue.config.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 
 module.exports = {
   chainWebpack: config => {},
-  /*devServer: {
+  devServer: {
     https: {
       key: fs.readFileSync('./certificates/ubii.private-key.pem'),
       cert: fs.readFileSync('./certificates/ubii.cert.pem'),
     },
     public: 'https://localhost:8080/'
-  }*/
+  }
 };
